f8b129959443d0f02e6edea446ad34eb5ab6083f
William Chong <me@williamchong.cloud>
2024-06-28 08:31:20 +0800

    ✨ Add json and cbor conversion

--- app.vue
+++ app.vue
@@ -1,6 +1,79 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1>CBOR to JSON Online Converter</h1>
+    <p>Convert CBOR from/to JSON string</p>
+    <section style="display: flex; flex-wrap: wrap;">
+      <div style="flex: 1">
+        <div style="display: flex; gap: 25%;">
+          <label for="cbor-value">CBOR</label>
+          <div>
+            <label for="cbor-encoding">Encoding</label>
+            <select id="cbor-encoding" v-model="cborEncoding">
+              <option value="base64">base64</option>
+              <option value="hex">hex</option>
+            </select>
+          </div>
+        </div>
+        <textarea id="cbor-value" @input="cborToJson" v-model="cborValue" />
+      </div>
+      <div style="flex: 1">
+        <label for="json-value">JSON</label>
+        <br />
+        <textarea id="json-value" @input="jsonToCbor" v-model="jsonValue" />
+      </div>
+    </section>
   </div>
 </template>
+
+<script setup lang="ts">
+import { decode, encode } from 'cbor-x';
+import { Buffer } from 'node:buffer'
+
+const cborValue = ref('')
+const jsonValue = ref(JSON.stringify({
+  hello: 'world',
+  array: [1, 2, 3],
+  nested: {
+    key: 'value'
+  }
+}))
+const cborEncoding = ref('base64' as BufferEncoding)
+
+onMounted(() => {
+  jsonToCbor()
+})
+function stringToBuffer(input: string) {
+  if (cborEncoding.value === 'base64') {
+    return Buffer.from(input, 'base64')
+  } else {
+    return Buffer.from(input, 'hex')
+  }
+}
+
+function cborToJson() {
+  try {
+    const cbor = decode(stringToBuffer(cborValue.value))
+    jsonValue.value = JSON.stringify(cbor, null, 2)
+  } catch (e) {
+    jsonValue.value = (e as Error).message
+  }
+}
+
+function jsonToCbor() {
+  try {
+    const cbor = encode(JSON.parse(jsonValue.value))
+    cborValue.value = Buffer.from(cbor).toString(cborEncoding.value)
+  } catch (e) {
+    cborValue.value = (e as Error).message
+  }
+}
+
+watch(cborEncoding, () => jsonToCbor())
+</script>
+
+<style scoped>
+textarea {
+  width: 100%;
+  min-height: 300px;
+}
+</style>
