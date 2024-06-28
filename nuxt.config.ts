fe1db1d31bcda9827e330e89bfa7a021a2e34517
William Chong <me@williamchong.cloud>
2024-06-28 09:05:44 +0800

    📈 Add gtag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ["nuxt-gtag"],
+  gtag: {
+    id: "G-PG6KXFFZPF",
+  },
   devtools: { enabled: true },
   experimental: {
     clientNodeCompat: true,
   },
-})
+});
