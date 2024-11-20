d62798edb95bad1ed52747ad757c3c03b18123ec
William Chong <me@williamchong.cloud>
2024-11-20 08:02:38 +0800

    ⚡️ Preload gtag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,8 +7,12 @@ export default defineNuxtConfig({
         { name: 'og:title', content: 'CBOR to JSON Online Converter' },
         { name: 'description', content: 'Easily convert data between CBOR (Concise Binary Object Representation) and JSON (JavaScript Object Notation) formats with this online web decoder.' }
       ],
+      link: [
+        { rel: 'preload', as: 'script', href: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5384552213891537', crossorigin: 'anonymous' },
+        { rel: 'preload', as: 'script', href: 'https://www.googletagmanager.com/gtag/js?id=G-PG6KXFFZPF' },
+      ],
       script: [
-        { src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5384552213891537', async: true, crossorigin: 'anonymous' },
+        { src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5384552213891537', async: true, crossorigin: 'anonymous', body: true },
       ],
     },
   },
