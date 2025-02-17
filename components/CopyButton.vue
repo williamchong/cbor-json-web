13b85af5a26a29e825053983503afb20773e79de
William Chong <me@williamchong.cloud>
2025-02-18 00:11:35 +0800

    💄 Use icon to replace svg

--- components/CopyButton.vue
+++ components/CopyButton.vue
@@ -1,28 +1,18 @@
 <template>
   <button
-    class="absolute top-2 right-2 p-2 text-gray-500 hover:text-gray-700 bg-white/80 rounded-lg shadow-sm border border-gray-200"
+    class="absolute top-2 right-2 p-1 text-gray-500 hover:text-gray-700 bg-white/80 rounded-lg shadow-sm border border-gray-200"
     @click="copyText"
   >
-    <svg
+    <Icon
       v-if="!copied"
-      class="w-5 h-5"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-    >
-      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" />
-    </svg>
-    <svg
+ s     name="material-symbols:content-copy-outline"
+      class="mt-1 w-4 h-4"
+    />
+    <Icon
       v-else
-      class="w-5 h-5"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-    >
-      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
-    </svg>
+      name="material-symbols:check-circle-outline"
+      class="mt-1 w-4 h-4"
+    />
   </button>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -8,19 +8,7 @@
         <div class="flex-1">
           <div class="flex justify-between items-center mb-2">
             <label for="cbor-value" class="text-sm font-medium text-gray-700">CBOR</label>
-            <div class="flex items-center gap-2">
-              <button
-                class="px-3 py-1 text-sm rounded-lg border border-gray-300 hover:bg-gray-100"
-                @click="$refs.fileInput.click()"
-              >
-                Upload Binary
-              </button>
-              <input
-                ref="fileInput"
-                type="file"
-                class="hidden"
-                @change="handleFileUpload"
-              >
+            <div class="flex items-center gap-4 mx-2">
               <label for="cbor-encoding" class="text-sm font-medium text-gray-700">Encoding</label>
               <select
                 id="cbor-encoding"
@@ -30,6 +18,21 @@
                 <option value="base64">base64</option>
                 <option value="hex">hex</option>
               </select>
+              <button
+                class="p-1 rounded-lg hover:bg-gray-100"
+                @click="$refs.fileInput.click()"
+              >
+                <Icon
+                  name="material-symbols:upload-file"
+                  class="w-4 h-4"
+                />
+              </button>
+              <input
+                ref="fileInput"
+                type="file"
+                class="hidden"
+                @change="handleFileUpload"
+              >
             </div>
           </div>
           <div class="relative">
@@ -44,7 +47,7 @@
           </div>
         </div>
         <div class="flex-1">
-          <div class="flex justify-between items-center mb-2">
+          <div class="flex justify-between items-center mb-2 mx-2">
             <label for="json-value" class="text-sm font-medium text-gray-700">JSON</label>
             <div v-if="!isJsonInput" class="flex items-center gap-4 relative">
               <div class="flex items-center gap-2">
@@ -61,13 +64,13 @@
                 </select>
               </div>
               <button
-                class="px-2 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                class="p-1 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500"
                 @click="isSettingsOpen = !isSettingsOpen"
               >
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z" />
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                </svg>
+                <Icon
+                  name="material-symbols:settings"
+                  class="w-4 h-4"
+                />
               </button>
               <div
                 v-show="isSettingsOpen"
