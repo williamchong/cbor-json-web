5c60d46e9994a30c3ace0aa67ca1258a8e17008b
William Chong <me@williamchong.cloud>
2025-02-22 05:28:23 +0800

    🌐 Add zh translate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -32,11 +32,11 @@ export default defineNuxtConfig({
         language: 'en-US',
         file: 'en.json'
       },
-      // {
-      //   code: 'zh',
-      //   language: 'zh-TW',
-      //   file: 'zh.json'
-      // }
+      {
+        code: 'zh',
+        language: 'zh-TW',
+        file: 'zh.json'
+      }
     ],
     lazy: true,
     defaultLocale: 'en',
