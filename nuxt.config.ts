1c33a8f1d91c22cc4e195c169333a4e6bec5e885
William Chong <me@williamchong.cloud>
2026-02-05 20:29:51 +0800

    ⚡️ Remove gtag preload to improve LCP
    The nuxt-gtag module already handles script loading; preloading the
    analytics script competes for bandwidth with critical resources.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-    head: {
-      link: [
-        { rel: 'preload', as: 'script', href: 'https://www.googletagmanager.com/gtag/js?id=G-PG6KXFFZPF' },
-      ],
-    },
-  },
-
   modules: [
     'nuxt-gtag',
     '@nuxtjs/i18n',
